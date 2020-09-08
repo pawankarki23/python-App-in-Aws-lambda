0d579db1b5d02945cb7f580c763398a85ef415dd
pawankarki23 <pawankarki5@gmail.com>
2020-09-08 00:11:46 -0500

    adding new files

---
+++ helloworld/lambda_function.py
@@ -0,0 +1,18 @@
+# importing libraries that we will be using in our lambda code
+import json
+
+# entry point of lambda function or main function 
+def lambda_handler(event, context):
+    # TODO implement
+    
+    # create a message from event data
+    message = event['key1'] + event['key2'] + event['key3']
+    
+    #print the message
+    print(message)
+    
+    # leave the return code as it is
+    return {
+        'statusCode': 200,
+        'body': json.dumps(message)
+    }
---
+++ image-resizer-aws-lambda/lambda_function.py
@@ -0,0 +1,56 @@
+import urllib.parse
+# AWS SDK for python
+import boto3
+# os library
+import os
+# create unique IDs 
+import uuid
+
+# import Image module from Pillow
+from PIL import Image 
+
+# initialize the S3 object
+s3 = boto3.client('s3')
+     
+
+print('Loading image resize lambda function')
+
+
+def resize_image(image_path, resized_path):
+    with Image.open(image_path) as image:
+        image.thumbnail((128, 128))
+        image.save(resized_path)
+     
+
+def lambda_handler(event, context):
+    # Let us print the event so that we can see how it looks 
+    print(event)
+    
+    # Get the triggering bucket name from the event 
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    print(bucket)
+
+    #Get the file/key name from the event
+    key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
+    
+    # save the file in /tmp directory -- lambda provide /tmp directory for storage during the execution of function
+    temp_save_path = '/tmp/{}{}'.format(uuid.uuid4(), key)
+    print('temp_save_path ', temp_save_path)
+    
+    # create a resized filename 
+    temp_resize_path = '/tmp/resized-{}'.format(key)
+    print('resized filename ', temp_resize_path)
+    
+    # Now dowload the file from s3 to /tmp directory
+    s3.download_file(bucket, key, temp_save_path)
+    print('Dirs in /tmp ', os.listdir('/tmp/'))
+    
+    # resize the image -- create image thumbnail
+    resize_image(temp_save_path, temp_resize_path)
+    print('Dirs in /tmp ', os.listdir('/tmp/'))
+
+    # upload resized image to destination bucket
+    dest_bucket = '{}-resized'.format(bucket)
+    s3.upload_file(temp_resize_path,dest_bucket , key)
+    
+    print('Done with uploading file to dest bucket ', dest_bucket)
